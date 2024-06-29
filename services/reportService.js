121ebdad10d74c4166a57acd5e966c7bb83ca31b
Amjubackit <110063824+Amjubackit@users.noreply.github.com>
2024-06-30 01:12:19 +0300

    make sure getReport user_id is valid

--- services/reportService.js
+++ services/reportService.js
@@ -1,8 +1,11 @@
 const calorieService = require('./calorieService');
+const { getUserById } = require('./userService');
 
 // Generate report - service layer
 const generateReport = async (user_id, year, month) => {
     try {
+        // Make sure such user exists first.
+        await getUserById(user_id);
         // Utilize calories service to get the relevant calorie items.
         const calories = await calorieService.getCaloriesByUserAndDate(
             user_id,
